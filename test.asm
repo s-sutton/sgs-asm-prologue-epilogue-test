7f795a45bfcffb33e2d05aa74afe410013458402
S. Sutton <60745579+s-sutton@users.noreply.github.com>
2023-11-27 13:57:03 -0600

    Update test.asm
    Maybe this fixes the hex formats?

--- test.asm
+++ test.asm
@@ -7,27 +7,27 @@
 // prologue for main()
 push ebp
 mov ebp,esp
-sub esp,20
+sub esp,0x20
 
 // main() function
-mov dword ptr ss:[esp+1C],0B
-mov dword ptr ss:[esp+18],0C
-mov dword ptr ss:[esp+14],0D
+mov dword ptr ss:[esp+0x1C],0x0B
+mov dword ptr ss:[esp+0x18],0x0C
+mov dword ptr ss:[esp+0x14],0x0D
 
 // parameters for calling functest(30, 31, 32)
-mov dword ptrs ss:[esp+4],1F
-mov dword ptr ss:[esp],1E
+mov dword ptrs ss:[esp+0x04],0x1F
+mov dword ptr ss:[esp],0x1E
 
 //define EIP?
 
 //prologue for functest()
 push ebp
 mov ebp,esp
-sub esp,10
+sub esp,0x10
 
 // void functest(int a, int b, int c)
-mov dword ptr ss:[esp+number],37
-mov dword ptr ss:[esp+number],38
+mov dword ptr ss:[esp+number],0x37
+mov dword ptr ss:[esp+number],0x38
 
 //epilogue for functest()
 mov esp,ebp
