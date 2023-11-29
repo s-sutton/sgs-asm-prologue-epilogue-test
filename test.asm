0d21995652f7d3c35efd46fd1b70a6cefdabb577
S. Sutton <60745579+s-sutton@users.noreply.github.com>
2023-11-29 11:21:13 -0600

    Update test.asm, fixed comments

--- test.asm
+++ test.asm
@@ -1,40 +1,40 @@
-/.// WIP to make a functional script out of the example code given in the Penetration Testing Professional course from INE
+; /.// WIP to make a functional script out of the example code given in the Penetration Testing Professional course from INE
 
-/.// paramaters for main(int argc, char argv*[])
+; /.// paramaters for main(int argc, char argv*[])
 
-/.// define EIP?
+; /.// define EIP?
 
-/.// prologue for main()
+; /.// prologue for main()
 PUSH EBP
 MOV EBP,ESP
 SUB ESP,0x20
 
-/.// main() function
+; /.// main() function
 MOV DWORD PTR SS:[ESP+0x1C],0x0B
 MOV DWORD PTR SS:[ESP+0x18],0x0C
 MOV DWORD PTR SS:[ESP+0x14],0x0D
 
-/.// parameters for calling functest(30, 31, 32)
+; /.// parameters for calling functest(30, 31, 32)
 MOV DWORD PTR SS:[ESP+0x04],0x1F
 MOV DWORD PTR SS:[ESP],0x1E
 
-/.//define EIP?
+; /.//define EIP?
 
-/.//prologue for functest()
+; /.//prologue for functest()
 PUSH EBP
 MOV EBP,ESP
 SUB ESP,0x10
 
-/.// void functest(int a, int b, int c)
+; /.// void functest(int a, int b, int c)
 MOV DWORD PTR SS:[ESP+0x04],0x37
 MOV DWORD PTR SS:[ESP],0x38
 
-/.//epilogue for functest()
+; /.//epilogue for functest()
 MOV ESP,EBP
 POP EBP
 RET ; probably not going to do anything since there is no call
 
-/.//epilogue for main()
+; /.//epilogue for main()
 MOV ESP,ENP
 POP EBP
 RET ; also not going to do anything without a call, probably?
