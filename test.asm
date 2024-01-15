bb54c902296afbaa056185ba1991ecfd82cfefeb
S. Sutton <60745579+s-sutton@users.noreply.github.com>
2024-01-15 10:34:40 -0600

    Update test.asm, restructured functions for EIP/RET
    Hopefully this can illustrate how subroutines work after some reformatting.

--- test.asm
+++ test.asm
@@ -5,15 +5,26 @@
 
 ; /.// WIP to make a functional script out of the unfinished, partial example code given in the Penetration Testing Professional course from INE
 
-_main:
-; /.// paramaters for main(int argc, char argv*[])
-PUSH 0x0
-PUSH 0x0
-; initialized parameters to 0 because they have no value, which I think is normally the default in C?
+_functest:
+; /.//prologue for functest()
+PUSH EBP
+MOV EBP,ESP
+SUB ESP,0x07
 
-:/.//define EIP
-; PUSH EIP
+; /.// void functest(int a, int b, int c) { int test1 = 55; int test2 = 56; }
+MOV DWORD PTR SS:[ESP+0x04],0x38
+MOV DWORD PTR SS:[ESP],0x39
 
+; /.//epilogue for functest()
+; reset the stack pointer to point to the base, which was defined at the beginning of the function, so we are now pointing back to the beginning of functest()
+MOV ESP,EBP
+; by popping the base pointer, we are restoring its value to the previous base, which is at the beginning of main()
+; this is because we stored the value of the previous base at the beginning of the new base during the prologue, so we are now popping that stored value back into the base pointer register
+POP EBP
+; return essentially pops the value from the stack into the EIP and then jumps to it, which in this case would be the old EIP if defined earlier
+RET
+
+_mainfunc:
 ; /.// prologue for main()
 ;save current base pointer for later use
 PUSH EBP
@@ -34,41 +45,38 @@ MOV DWORD PTR SS:[ESP],0x0D
 
 ; the rest is mostly the same process as above
 
-_functest:
 ; /.// parameters for calling functest(30, 31, 32)
 PUSH 0x20
 PUSH 0x1F
 PUSH 0x1E
 
 ; /.// define EIP
-; PUSH EIP
+; I have no idea how to actually do this. I know that if I merely push the EIP, then when it is called again I will essentially create an infinite loop
+; So I have to push the EIP pointing to the next instruction rather than the current one, and I think every instruction is loaded into memory with the program, with 4 bytes for each instruction so
+MOV RAX,EIP
+ADD RAX,16
+PUSH RAX
+JMP functest
+; This should push an EIP value that is just after JMP functest, right?
 
-; /.//prologue for functest()
-PUSH EBP
-MOV EBP,ESP
-SUB ESP,0x07
-
-; /.// void functest(int a, int b, int c) { int test1 = 55; int test2 = 56; }
-MOV DWORD PTR SS:[ESP+0x04],0x38
-MOV DWORD PTR SS:[ESP],0x39
-
-; /.//epilogue for functest()
-; reset the stack pointer to point to the base, which was defined at the beginning of the function, so we are now pointing back to the beginning of functest()
-MOV ESP,EBP
-; by popping the base pointer, we are restoring its value to the previous base, which is at the beginning of main()
-; this is because we stored the value of the previous base at the beginning of the new base during the prologue, so we are now popping that stored value back into the base pointer register
-POP EBP
-; return essentially pops the value from the stack into the EIP and then jumps to it, which in this case would be the old EIP if defined earlier
-; commented out because this might actually break the program here since I am not using syscalls to shift the instruction pointer between functions, but it's important to keep for illustrative purposes
-; RET
-
-_epilogue:
 ; /.//epilogue for main()
 ; reset the stack pointer to point to the base, which was defined at the beginning of the function, so now we are pointing to the beginning of main()
 MOV ESP,EBP
 ; this time, when we pop the base pointer, it will now go back to the value it had before it was set to the beginning of main(), which should be(?) the beginning of the entire program
 POP EBP
-; RET
+RET
+
+_main:
+; /.// paramaters for main(int argc, char argv*[])
+PUSH 0x0
+PUSH 0x0
+; initialized parameters to 0 because they have no value, which I think is normally the default in C?
+
+:/.//define EIP
+MOV RAX,EIP
+ADD RAX,16
+PUSH RAX
+JMP mainfunc
 
 ;/.//exit the program
 ; the syscall reads from the accumulator to determine what system instruction to execute, here 0x60 corresponds to sys_exit
